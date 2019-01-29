2380430b35dcac3d951828e4d8eea0075e022ca9
Andre Baltieri <hello@balta.io>
2019-01-29 00:19:14 -0200

    Iniciado repo

---
+++ src/app/app.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
+
+@Component({
+  selector: 'app-root', // <app-root>
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public todos: Todo[] = [];
+  public title: String = 'Minhas Tarefas';
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required,
+      ])]
+    });
+
+    this.load();
+  }
+
+  add() {
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+  }
+
+  clear() {
+    this.form.reset();
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+    this.save();
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+    this.save();
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+    this.save();
+  }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    this.todos = JSON.parse(data);
+  }
+}
---
+++ src/models/todo.model.ts
@@ -0,0 +1,7 @@
+export class Todo {
+    constructor(
+        public id: Number,
+        public title: String,
+        public done: Boolean,
+    ) { }
+}
