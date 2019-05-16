341d8873ebf26f0398f48fd6eea0ecfae52e1608
Andre Baltieri <andrebaltieri@hotmail.com>
2019-05-16 11:35:39 -0300

    add checkbox, input, radio, slide

--- src/material/src/app/app.module.ts
+++ src/material/src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatAutocompleteModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatAutocompleteModule, MatFormFieldModule, MatInputModule, MatCheckboxModule, MatRadioModule, MatSlideToggleModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AutoCompleteComponent } from './components/auto-complete/auto-complete.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CheckInputRadioSlideComponent } from './components/check-input-radio-slide/check-input-radio-slide.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AutoCompleteComponent
+    AutoCompleteComponent,
+    CheckInputRadioSlideComponent
   ],
   imports: [
     BrowserModule,
@@ -20,6 +22,9 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatInputModule,
     MatAutocompleteModule,
     MatFormFieldModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSlideToggleModule,
     AppRoutingModule
   ],
   providers: [],
