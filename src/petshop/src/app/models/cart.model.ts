bc3ad60888c9d8ca2e7a43787c71576efbc83aa5
Renan Esposte <renan.esposte@gmail.com>
2019-08-06 17:29:42 -0300

    Desmembrando curso em 7181 e 7187

--- src/petshop/src/app/models/cart.model.ts
+++
@@ -1,9 +0,0 @@
-import { CartItem } from './cart-item';
-
-export class Cart {
-    constructor(
-        public items: CartItem[] = []
-    ) {
-
-    }
-}
--- src/petshop/src/app/models/product.model.ts
+++
@@ -1,8 +0,0 @@
-export class Product {
-    public _id: string;
-    public title: string;
-    public category: string;
-    public description: string;
-    public price: number;
-    public images: string[];
-}
